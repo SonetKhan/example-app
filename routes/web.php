65bcd2cee412be4bfeff75c5ea5fe3bba51dd625
sonetkhan <sonetkhanrkb@gmail.com>
2021-08-30 14:44:04 +0600

    update route
    complete project

--- routes/web.php
+++ routes/web.php
@@ -41,34 +41,37 @@ use App\Http\Controllers\CategoryController;
 
 
 Route::get('/', function () {
+
     $brands = DB::table('brands')->get();
+
     $aboutDetails = DB::table('home_abouts')->get();
+
     $multiImages = DB::table('multi_pics')->get();
-    return view('home',compact('brands','aboutDetails','multiImages'));
+
+    return view('home', compact('brands', 'aboutDetails', 'multiImages'));
 });
 
-Route::get('/about',function(){
+Route::get('/about', function () {
 
     return view('about');
 });
 
 
 
-Route::get('/index',[contactController::class,'index'])->name('contact');
+Route::get('/index', [contactController::class, 'index'])->name('contact');
 
-Route::get('/home',function(){
+Route::get('/home', function () {
 
     echo "this is home page";
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
-//    $users = User::all();
+    //    $users = User::all();
 
-//    $users = DB::table('users')->get();
+    //    $users = DB::table('users')->get();
 
     return view('admin.index');
-
 })->name('dashboard');
 
 /*
@@ -78,19 +81,19 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 */
 
 
-Route::get('/category/all',[CategoryController::class,'allCategory'])->name('all.category');
+Route::get('/category/all', [CategoryController::class, 'allCategory'])->name('all.category');
 
-Route::post('/category/add',[CategoryController::class,'addCategory'])->name('add.category');
+Route::post('/category/add', [CategoryController::class, 'addCategory'])->name('add.category');
 
-Route::get('/category/edit/{id}',[CategoryController::class,'editCategory']);
+Route::get('/category/edit/{id}', [CategoryController::class, 'editCategory']);
 
-Route::post('/category/update/{id}',[CategoryController::class,'updateCategory']);
+Route::post('/category/update/{id}', [CategoryController::class, 'updateCategory']);
 
-Route::get('/category/softDelete/{id}',[CategoryController::class,'softDelete']);
+Route::get('/category/softDelete/{id}', [CategoryController::class, 'softDelete']);
 
-Route::get('/category/restore/{id}',[CategoryController::class,'restoreCategory']);
+Route::get('/category/restore/{id}', [CategoryController::class, 'restoreCategory']);
 
-Route::get('/category/permanentDelete/{id}',[CategoryController::class,'permanentDelete']);
+Route::get('/category/permanentDelete/{id}', [CategoryController::class, 'permanentDelete']);
 
 /*
 |--------------------------------------------------------------------------
@@ -98,15 +101,15 @@ Route::get('/category/permanentDelete/{id}',[CategoryController::class,'permanen
 |--------------------------------------------------------------------------
 */
 
-Route::get('/brand/all',[BrandController::class,'allBrand'])->name('all.brand');
+Route::get('/brand/all', [BrandController::class, 'allBrand'])->name('all.brand');
 
-Route::post('/brand/add',[BrandController::class,'addBrand'])->name('add.brand');
+Route::post('/brand/add', [BrandController::class, 'addBrand'])->name('add.brand');
 
-Route::get('/brand/edit/{id}',[BrandController::class,'editBrand']);
+Route::get('/brand/edit/{id}', [BrandController::class, 'editBrand']);
 
-Route::post('/brand/update/{id}',[BrandController::class,'updateBrand']);
+Route::post('/brand/update/{id}', [BrandController::class, 'updateBrand']);
 
-Route::get('/brand/delete/{id}',[BrandController::class,'deleteBrand']);
+Route::get('/brand/delete/{id}', [BrandController::class, 'deleteBrand']);
 
 /*
 |--------------------------------------------------------------------------
@@ -115,9 +118,9 @@ Route::get('/brand/delete/{id}',[BrandController::class,'deleteBrand']);
 */
 
 
-Route::get('/multi/image',[BrandController::class,'multiPic'])->name('multi.image');
+Route::get('/multi/image', [BrandController::class, 'multiPic'])->name('multi.image');
 
-Route::post('/multi/image/store',[BrandController::class,'storeImage'])->name('store.image');
+Route::post('/multi/image/store', [BrandController::class, 'storeImage'])->name('store.image');
 
 /*
 |--------------------------------------------------------------------------
@@ -125,7 +128,7 @@ Route::post('/multi/image/store',[BrandController::class,'storeImage'])->name('s
 |--------------------------------------------------------------------------
 */
 
-Route::get('user/logout',[BrandController::class,'Logout'])->name('user.logout');
+Route::get('user/logout', [BrandController::class, 'Logout'])->name('user.logout');
 
 
 
@@ -134,28 +137,28 @@ Route::get('user/logout',[BrandController::class,'Logout'])->name('user.logout')
 | Admin All route
 |--------------------------------------------------------------------------
 */
-Route::get('/home/slider',[HomeController::class,'homeSlider'])->name('home.slider');
+Route::get('/home/slider', [HomeController::class, 'homeSlider'])->name('home.slider');
 
-Route::get('/home/add/slider',[HomeController::class,'addSlider'])->name('add.slider');
+Route::get('/home/add/slider', [HomeController::class, 'addSlider'])->name('add.slider');
 
-Route::post('/home/store/slider',[HomeController::class,'storeSlider'])->name('store.slider');
+Route::post('/home/store/slider', [HomeController::class, 'storeSlider'])->name('store.slider');
 
 /*
 |--------------------------------------------------------------------------
 | Home About All route
 |--------------------------------------------------------------------------
 */
-Route::get('/home/about',[AboutController::class,'about'])->name('home.about');
+Route::get('/home/about', [AboutController::class, 'about'])->name('home.about');
 
-Route::get('home/about/add',[AboutController::class,'addAbout'])->name('add.about');
+Route::get('home/about/add', [AboutController::class, 'addAbout'])->name('add.about');
 
-Route::post('home/about/store',[AboutController::class,'storeAbout'])->name('store.about');
+Route::post('home/about/store', [AboutController::class, 'storeAbout'])->name('store.about');
 
-Route::get('home/about/edit/{id}',[AboutController::class,'editAbout']);
+Route::get('home/about/edit/{id}', [AboutController::class, 'editAbout']);
 
-Route::post('home/about/update/{id}',[AboutController::class,'updateAbout'])->name('update.about');
+Route::post('home/about/update/{id}', [AboutController::class, 'updateAbout'])->name('update.about');
 
-Route::get('home/about/delete/{id}',[AboutController::class,'aboutDelete']);
+Route::get('home/about/delete/{id}', [AboutController::class, 'aboutDelete']);
 
 /*
 |--------------------------------------------------------------------------
@@ -163,25 +166,25 @@ Route::get('home/about/delete/{id}',[AboutController::class,'aboutDelete']);
 |--------------------------------------------------------------------------
 */
 
-Route::get('/portfolio',[PortfolioController::class,'portfolioDetails'])->name('portfolio');
+Route::get('/portfolio', [PortfolioController::class, 'portfolioDetails'])->name('portfolio');
 /*
 |--------------------------------------------------------------------------
 | Contact page route
 |--------------------------------------------------------------------------
 */
-Route::get('/admin/contact',[contactController::class,'adminContact'])->name('admin.contact');
+Route::get('/admin/contact', [contactController::class, 'adminContact'])->name('admin.contact');
 
-Route::get('/admin/contact/add',[contactController::class,'addContact'])->name('add.contact');
+Route::get('/admin/contact/add', [contactController::class, 'addContact'])->name('add.contact');
 
-Route::post('admin/contact/store',[contactController::class,'storeContact'])->name('store.contact');
+Route::post('admin/contact/store', [contactController::class, 'storeContact'])->name('store.contact');
 
-Route::get('/home/contact',[contactController::class,'contactDetails'])->name('contact');
+Route::get('/home/contact', [contactController::class, 'contactDetails'])->name('contact');
 
-Route::post('/home/contact/message',[contactController::class,'contactMessage'])->name('contact.message');
+Route::post('/home/contact/message', [contactController::class, 'contactMessage'])->name('contact.message');
 
-Route::get('/admin/contact/message',[contactController::class,'adminMessage'])->name('admin.message');
+Route::get('/admin/contact/message', [contactController::class, 'adminMessage'])->name('admin.message');
 
-Route::get('/admin/message/delete/{id}',[contactController::class,'adminMessageDelete']);
+Route::get('/admin/message/delete/{id}', [contactController::class, 'adminMessageDelete']);
 
 /*
 |--------------------------------------------------------------------------
@@ -189,7 +192,6 @@ Route::get('/admin/message/delete/{id}',[contactController::class,'adminMessageD
 |--------------------------------------------------------------------------
 */
 
-Route::get('/user/password',[\App\Http\Controllers\ChangePassController::class,'changePassword'])->name('change.password');
+Route::get('/user/password', [\App\Http\Controllers\ChangePassController::class, 'changePassword'])->name('change.password');
 
-Route::post('/user/password/update',[\App\Http\Controllers\ChangePassController::class,'updatePassword'])->name('password.update');
-
+Route::post('/user/password/update', [\App\Http\Controllers\ChangePassController::class, 'updatePassword'])->name('password.update');
